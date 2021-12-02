0cd78911ae3b6762844ff56d5ea2c5ad3e7b7118
Thomas <thomas.jegou1@gmail.com>
2021-12-02 10:12:37 +0100

    Init project TP2

---
+++ src/main/java/fr/tjegou/tp_2_prg_jsf/JAXRSConfiguration.java
@@ -0,0 +1,13 @@
+package fr.tjegou.tp_2_prg_jsf;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ * Configures JAX-RS for the application.
+ * @author Juneau
+ */
+@ApplicationPath("resources")
+public class JAXRSConfiguration extends Application {
+    
+}
